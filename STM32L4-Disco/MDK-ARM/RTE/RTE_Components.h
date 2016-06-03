fdea5ffe01b93618cdae22e20f197628c51ddf1b
becki2k <becki2k@gmx.de>
2016-06-03 23:49:16 +0200

    added

---
+++ STM32L4-Disco/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32L4-Disco' 
+ * Target:  'STM32L4-Disco' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
