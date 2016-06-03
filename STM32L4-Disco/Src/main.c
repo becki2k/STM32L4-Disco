5eeabe1dd06132a4936a3dcee4e2d019e48208d1
becki2k <becki2k@gmx.de>
2016-06-03 23:58:25 +0200

    added

--- STM32L4-Disco/Src/main.c
+++ STM32L4-Disco/Src/main.c
@@ -37,6 +37,7 @@
 
 
 /* USER CODE BEGIN Includes */
+#include "stdio.h"
 #include "L3GD20.h"
 #define ITM_Port8(n)    (*((volatile unsigned char *)(0xE0000000+4*n)))
 #define ITM_Port16(n)   (*((volatile unsigned short*)(0xE0000000+4*n)))
